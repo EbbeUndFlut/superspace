7ce7228471878999adf23624e51eeb2bdf05f5c8
Christian <c.peters@makermaker.de>
2022-09-09 14:48:02 +0200

    magda rockt das jetzt

---
+++ src/components/spacedisplay/SpaceDisplay.jsx
@@ -0,0 +1,9 @@
+const SpaceDisplay = (props) => {
+    return ( 
+        <section>
+            <img src={props.img} alt="" />
+        </section>
+     );
+}
+ 
+export default SpaceDisplay;
--- src/pages/custom/Custom.jsx
+++ src/pages/custom/Custom.jsx
@@ -1,5 +1,17 @@
+import SpaceDisplay from "../../components/spacedisplay/SpaceDisplay";
+
 const Custom = () => {
-    return ( <h1>custom</h1> );
+    return ( <main>
+        <section>
+            <label htmlFor="">Long</label>
+            <input type="number" name="" id="" />
+            <label htmlFor="">Lat</label>
+            <input type="number" name="" id="" />
+            <button>Zörtsch</button>
+        <button>back</button>
+        </section>
+        <SpaceDisplay img={}></SpaceDisplay>
+    </main> );
 }
  
 export default Custom;
